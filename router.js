e1adfdda3df67ae99b77bebb40e212befd2d368e
liuchenwei-echo <liuchenwei_echo@163.com>
2015-03-09 18:41:16 +0800

    首页
    首页。文章列表。

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var log4js = require("log4js");
 var ejs = require("ejs");
+var cfg = require("./article.json");
 //var json = require("./router-cfg.json");
 var logger = log4js.getLogger();
 var root = "";
@@ -18,17 +19,20 @@ module.exports={
 		// res.send(html);
 		
 		//使用ejs模板引擎进行渲染页面
-		res.render("index");
+		res.render("index",cfg);
 	},
 	"/article":function(req,res){
-		res.render("articleList");
+		res.render("articleList",cfg);
 	},
-	"/article/:articleName":function(req,res){
-		var articleName = req.params.articleName;
-		var title = "标题："+articleName;
+	"/article/:id":function(req,res){
+		var id = req.params.id;
+		var title = "标题："+id;
 		//var path = root +"tpl/article/"+id+".ejs";
 		//res.render():articleName
-		res.render("article",{articleName:articleName,title:title});
+		res.render("article",{id:id,title:title,type:"article"});
 		//res.send("hello " + articleName);
+	},
+	"/short":function(req,res){
+		res.render("shortList",cfg);
 	}
 }
